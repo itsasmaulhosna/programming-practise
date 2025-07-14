3919e5d1d209d5cc349c2a9e64e0f69b71264596
itsasmaulhosna <itsasmaulhosna@gmail.com>
2025-07-14 10:12:07 +0600

    condition1.c

---
+++ correct.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int x;
+    printf("Enter x=");
+    scanf("%d",&x);
+    //  while(x!=1999){
+        if(x==1999){
+            printf("correct\n");
+            // break;
+        }
+        else{
+            printf("wrong\n");
+            // break;
+        }
+    // }
+    return 0;
+}
