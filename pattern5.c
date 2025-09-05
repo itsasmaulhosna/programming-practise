c9ad2983de8f47810376700e55f46846be4c7661
itsasmaulhosna <itsasmaulhosna@gmail.com>
2025-09-05 11:39:25 +0600

    pattern.c

--- pattern5.c
+++ pattern5.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-int main(){
-    int n,row,col;
+int main()
+{
+    int n, row, col;
     printf("Enter n:");
-    scanf("%d",&n);
-    for(row=1;row<=n;row++){
-        for(col=1;col<=row;col++){
-            printf("%c",col+64);
+    scanf("%d", &n);
+    for (row = 1; row <= n; row++)
+    {
+        for (col = 1; col <= row; col++)
+        {
+            printf("%c", col + 64);
         }
         printf("\n");
     }
 
-
-    for(row=n-1;row>=1;row--){
-        for(col=1;col<=row;col++){
-            printf("%c",col+64);
+    for (row = n - 1; row >= 1; row--)
+    {
+        for (col = 1; col <= row; col++)
+        {
+            printf("%c", col + 64);
         }
         printf("\n");
     }
